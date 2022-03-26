aed86c71fa27fee1687eb83c6825d7616cdf876c
minhbang <minhbangod@vnu.edu.vn>
2022-03-26 18:23:28 +0700

    sadas

---
+++ index.js
@@ -0,0 +1,21 @@
+var axios = require('axios');
+var fs = require('fs')
+let first = 1, second = 2, operator = "^"
+var data = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"\r\n                  xmlns:gs="http://www.baeldung.com/springsoap/gen">\r\n    <soapenv:Header/>\r\n    <soapenv:Body>\r\n        <gs:CalculatorRequest>\r\n            <op>${operator}</op>\r\n            <first>${first}</first>\r\n            <second>${second}</second>\r\n        </gs:CalculatorRequest>\r\n    </soapenv:Body>\r\n</soapenv:Envelope>`;
+
+var config = {
+  method: 'post',
+  url: 'http://localhost:8080/ws/',
+  headers: { 
+    'Content-Type': 'text/xml'
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  fs.writeFileSync('response.xml', response.data)
+})
+.catch(function (error) {
+  console.log(error.message);
+});
---
+++ rest.js
@@ -0,0 +1,25 @@
+var axios = require('axios');
+var fs = require('fs')
+
+let op = {
+    plus: "plus",
+    minus : "minus",
+    multi: "multi",
+    divide: "divide",
+    pow : "pow"
+}
+
+let first = 1, second = 3, s = op.pow
+var config = {
+  method: 'get',
+  url: `http://localhost:8080/calculate?first=${first}&second=${second}&op=${s}`,
+};
+
+axios(config)
+.then(function (response) {
+console.log(response.data)
+  fs.writeFileSync('restRespone.json', JSON.stringify(response.data))
+})
+.catch(function (error) {
+  console.log(error.message);
+});
